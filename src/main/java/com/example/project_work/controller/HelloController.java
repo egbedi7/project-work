f9e79b9ef113062b54b44098711a2b50fa81f5f3
Unity imade <unity2caree@hotmail.com>
2026-08-12 04:36:07 -0600

    Update API response

--- src/main/java/com/example/project_work/controller/HelloController.java
+++ src/main/java/com/example/project_work/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from Project Work DevOps API!";
+        return "Hello from my firs e-commerce Project Work DevOps API!2";
     }
 }
